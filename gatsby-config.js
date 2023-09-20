44fb70f2a0bb01a9842b631dc5d8825bc2559539
@lemoner-rajesh <rajesh@lemonadverts.com>
2023-09-20 11:36:40 +0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `parsegatsby`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
